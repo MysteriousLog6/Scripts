b34f9cfd0e5c6fc869c6fd822b083eb660600dfb
Mysteriouslog6 <63236194+MysteriousLog6@users.noreply.github.com>
2022-01-20 19:03:59 +0000

    Update WhyredBuild.sh

--- LinuxKernel/WhyredBuild.sh
+++ LinuxKernel/WhyredBuild.sh
@@ -1,3 +1,7 @@
+red="\e[0;91m"
+green="\e[0;92m"
+reset="\e[0m"
+
 export ARCH=arm64 && export SUBARCH=arm64
 export CROSS_COMPILE="/home/mysteriouslog6/Kernel/gcc64/bin/aarch64-elf-"
 export CROSS_COMPILE_ARM32="/home/mysteriouslog6/Kernel/gcc32/bin/arm-eabi-"
